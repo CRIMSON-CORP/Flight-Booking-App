7bc8408a996b313b12897f12e71c3d34bbad580e
CRIMSON <olueatoworosanwo@gmail.com>
2022-11-01 03:08:27 +0100

    flights model

--- models/Flight.js
+++ models/Flight.js
@@ -1,1 +1,17 @@
-exports.exampleModel = [];
+/**
+ * @typedef Flight
+ * @property {string} id
+ * @property {string} title
+ * @property {string} time
+ * @property {number} price
+ * @property {string} date
+ * @property {string} createdAt
+ * @property {string} updatedAt
+ */
+
+/**
+ * @type {Flight[]}
+ */
+const FlightsModel = [];
+
+export default FlightsModel;
